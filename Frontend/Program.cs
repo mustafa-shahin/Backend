ad0659aac5cf39965e2287ba5c3d80c679cc0c86
Mustafa Shahin <109212127+mustafa-shahin@users.noreply.github.com>
2025-07-13 02:30:06 +0200

    Revert "services and interfaces for products, variants and categories"
    This reverts commit 92d1268f27d884fef6187734bd6590dba3974826.

--- Frontend/Program.cs
+++ Frontend/Program.cs
@@ -34,9 +34,6 @@ builder.Services.AddScoped<IPageService, PageService>();
 builder.Services.AddScoped<IUsersService, UsersService>();
 builder.Services.AddScoped<IFileService, FileService>();
 builder.Services.AddScoped<IFolderService, FolderService>();
-builder.Services.AddScoped<ICategoryService, CategoryService>();
-builder.Services.AddScoped<IProductService, ProductService>();
-builder.Services.AddScoped<IProductVariantService, ProductVariantService>();
 
 // Add UI services
 builder.Services.AddScoped<IThemeService, ThemeService>();
